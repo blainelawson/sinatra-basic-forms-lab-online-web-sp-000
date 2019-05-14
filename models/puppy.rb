ca50c9d9541ceadbaa9300d5d718fd623e90d069
Blaine Lawson <blaine.lawson@gmail.com>
2019-05-14 19:58:55 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,7 +1,7 @@
 class Puppy
   attr_reader :name, :breed, :age
 
-  def initialize(*{name: nil, breed: nil, months_old: nil})
+  def initialize(name: nil, breed: nil, months_old: nil)
     binding.pry
     @name = name
     @breed = breed
