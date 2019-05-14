82aaf3b1ecf1018efa82214507571102a1f26cc3
Blaine Lawson <blaine.lawson@gmail.com>
2019-05-14 20:05:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
     @name = params[:name]
     @breed = params[:breed]
     @months_old = params[:months_old]
-binding.pry
+
     @puppy = Puppy.new(name: @name, breed: @breed, age: @months_old)
 
     erb :display_puppy
