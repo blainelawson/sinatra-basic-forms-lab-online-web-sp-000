8e2a191bb44db6c1de09d9e6b465d103117544c4
Blaine Lawson <blaine.lawson@gmail.com>
2019-05-14 19:49:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,5 +1,11 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
+  post '/puppy' do
+    @name = params[:name]
+    @breed = params[:breed]
+    @age = params[:age]
+    
+    erb :display_puppy
+  end
 end
